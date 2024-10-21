a6f1bd8fa99ae85d8974445317b3f3a40190d5fd
Stephan Model <stephan.model@outlook.de>
2024-10-21 15:02:33 +0200

    Implement data fetch from vercel example API artists

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import useSWR from "swr";
 
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  async function fetcher(url) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+  if (error) return <h1>🛑 failed</h1>;
+  if (isLoading) return <h1>⏳ is loading</h1>;
+
+  const fetchData = data;
+
+  console.log(fetchData);
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Gallery App</h1>
     </div>
   );
 }
